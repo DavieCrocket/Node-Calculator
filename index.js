fef4bc039783e585fe45de5e0f9729632118071c
Doug Stensrud <dwsmitpython@gmail.com>
2023-11-29 07:22:56 -0800

    refactor following Andres' Loom. result is NaN.

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
 // Ask the user, "What operation would you like to perform?"
 // Then the user enters one of these options: "/" "*" "-" "+"
 // If the user enters an invalid character, print: "That is not a valid operation" and then restart the program
-// After the user enters a valid operation, ask the user, "Please enter the first number"
+const rs = require('readline-sync');
+
+const opObj = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+  '/': (a, b) => (a / b).toFixed(2),
+};
+
+function calculate(operationList, operation, firstNumber, secondNumber) {
+  return operationList[operation](firstNumber, secondNumber);
+}
+
+const getOperation = (limit) => {
+  return rs.question('What operation would you like to perform? ', {
+    limit,
+    limitMessage: "That is not a valid operation"
+  });
+};
+
+const getNumber = (str) => {
+  rs.questionInt(`Please enter the ${str} number `, {
+  limitMessage: 'This is not a number'
+});
+};
+
+const calculator = (opObj) => {
+  const opArr = Object.keys(opObj);
+  const operation = getOperation(opArr);
+  const [firstNumber, secondNumber] = ['first', 'second'].map((item) => 
+    getNumber(item)
+  );
+  const result = calculate(opObj, operation, firstNumber, secondNumber);
+  console.log(firstNumber);
+  console.log(`The result is ${result}`);
+};
+
+calculator(opObj);
+
+
+
+
 // The user then enters the first number. If the user enters something that is not a number, print: “This is not a number” and then re-ask the question
 // After a valid number is entered, ask the user, "Please enter the second number". Perform the same error handling as before
 // Then create a function to perform the proper math operation and print the result as: "The result is: X" where "X" is the actual result
-
-
-const rs = require('readline-sync');
-
-let result = 0;
-let numbers = [];
-let operation = 0;
-let firstNumber = 0;
-let secondNumber = 0;
-
-
-function calculate(firstNumber, secondNumber) {
-  if (operation === '+') {
-    return sum = firstNumber + secondNumber;
-  } else if (operation === '-') {
-    return difference = firstNumber - secondNumber;
-  } else if (operation === '*') {
-    return product = firstNumber * secondNumber;
-  } else if (operation === '/') {
-    let quotient = firstNumber / secondNumber;
-    return quotient.toFixed(2);
-  };
-}
-operation = rs.question('What operation would you like to perform? ', {
-  limit: [ '*', '/', '+', '-'],
-  limitMessage: "That is not a valid operation"
-});
-
-firstNumber = rs.questionInt("Please enter the first number ", {
-  limit: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-  limitMessage: 'This is not a number'
-});
-
-secondNumber = rs.questionInt("Please enter the second number ", {
-  limit: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-  limitMessage: 'This is not a number'
-});
-
-result = calculate(firstNumber, secondNumber);
-
-console.log('The result is ' + result);
